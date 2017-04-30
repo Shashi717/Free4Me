26e6771c2228e8aa3c60b0b6c87ef3b592eaf32e
Shashi <liyanagemadushani21@gmail.com>
2017-04-29 20:45:15 -0400

    Photo library access

--- Free4Me/Free4Me/PostViewController.swift
+++ Free4Me/Free4Me/PostViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
+import AVFoundation
+import AVKit
+import MobileCoreServices
 
 //let name: String
 //let image: String
@@ -17,33 +21,80 @@ import FirebaseDatabase
 //let borough: String
 //let expiration: String
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var categoryPickerView: UIPickerView!
+    @IBOutlet weak var datePickerView: UIDatePicker!
+    
+    @IBOutlet weak var pickedImageView: UIImageView!
+    var imagePickerController: UIImagePickerController!
+    
+    var capturedImage: UIImage!
     var user: User?
-    var databaseRef = FIRDatabase.database().reference()
+    var databaseRef: FIRDatabaseReference!
+    var storageRef: FIRStorageReference!
+    var userStore: UserStore?
+    
+    let categories = ["Books", "Furniture", "Tickets", "Electronics", "Other"]
+    var pickedCategory: String?
+    var pickedDate: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        categoryPickerView.delegate = self
+        categoryPickerView.dataSource = self
+        databaseRef = FIRDatabase.database().reference()
+        userStore = UserStore()
         
-
     }
-    
     
     func postToFirebase() {
         
+        let postRef = self.databaseRef.childByAutoId()
+        let storage = FIRStorage.storage()
+        let storageRef = storage.reference(forURL: "gs://free4me-49bcf.appspot.com")
+        let imageNameRef = storageRef.child("images/\(postRef.key)")
+        
+        let metadata = FIRStorageMetadata()
+        metadata.cacheControl = "public,max-age=300"
+        metadata.contentType = "image/jpeg"
+        
         if let userId = FIRAuth.auth()?.currentUser?.uid {
+            
+            if capturedImage != nil {
+                let jpeg = UIImageJPEGRepresentation(capturedImage, 0.5)
+                
+                let _ = imageNameRef.put(jpeg!, metadata: metadata, completion: { (metadata, error) in
+                    guard metadata != nil else {
+                        print("put error: \(String(describing: error?.localizedDescription))")
+                        return
+                    }
+                })
+                
+            }
+            
             var expiration = "N/A"
-            //            if let name = "" ,
-            //            let image = "",
-            //            let category = "",
-            //            let ownerId = "",
-            
-            let freebieDict = ["name": "", "image": "", "category": "", "ownerId": userId, "borough": "", "expiration":expiration] as [String:Any]
-            let postDict = ["name": "", "image": "", "category": "", "expiration":expiration] as [String:Any]
-            self.databaseRef.child("freebies").childByAutoId().setValue(freebieDict)
-            self.databaseRef.child((FIRAuth.auth()?.currentUser?.uid)!).setValue(postDict)
-            
+            if let name = nameTextField.text,
+                let category = pickedCategory,
+                let pickedDate = pickedDate {
+                expiration = pickedDate
+                userStore?.getUser(id: userId) { (user) in
+                    
+                    
+                    
+                    let freebieDict = ["name": name, "image": imageNameRef, "category": category, "ownerId": userId, "borough": user.borough, "expiration":expiration] as [String:Any]
+                    let postDict = ["name": "", "image": imageNameRef, "category": category, "expiration":expiration] as [String:Any]
+                    self.databaseRef.child("freebies").childByAutoId().setValue(freebieDict)
+                    self.databaseRef.child((FIRAuth.auth()?.currentUser?.uid)!).setValue(postDict)
+                    
+                }
+                
+            }
+            else {
+                print("not completed")
+            }
         }
         else {
             print("not signed in")
@@ -64,6 +115,78 @@ class PostViewController: UIViewController {
         
     }
     
+    func postImage(postRef: String, imageNameRef: String) {
+        
+        
+        
+    }
+    
+    
+    @IBAction func postButtonTapped(_ sender: UIButton) {
+        
+        self.postToFirebase()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    
+    //MARK: Delegates
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        pickedCategory = categories[row]
+        
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        switch info[UIImagePickerControllerMediaType] as! String {
+        case String(kUTTypeImage):
+            if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                self.capturedImage = image
+                pickedImageView.image = nil
+                pickedImageView.backgroundColor = .clear
+                pickedImageView.contentMode = .scaleAspectFit
+                pickedImageView.image = capturedImage
+            }
+            
+        default:
+            print("Bad Media Type")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        pickedDate = String(describing: datePickerView.date)
+    }
+    
+    @IBAction func pickImageTapped(_ sender: UIButton) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.modalPresentationStyle = .currentContext
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        imagePickerController.mediaTypes = [String(kUTTypeImage)]
+        
+        self.imagePickerController = imagePickerController
+        self.present(imagePickerController, animated: true, completion: nil)
+        
+
+    }
     
     /*
      // MARK: - Navigation
